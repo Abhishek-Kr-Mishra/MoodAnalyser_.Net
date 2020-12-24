c6986f76002f6618658d00b69d1d9a17ee7800a7
abh224272 <abh224272@gmail.com>
2020-12-24 17:58:14 +0530

    [Abhishek Mishra] Add: T.C 1.2 Message should return Happy Mood

--- MoodAnalyzerUnitTest/MoodAnalyserUnitTestClass.cs
+++ MoodAnalyzerUnitTest/MoodAnalyserUnitTestClass.cs
@@ -7,12 +7,21 @@ namespace MoodAnalyzerUnitTest
     public class MoodAnalyserUnitTestClass
     {
         [TestMethod]
-        public void GivenMood_ShouldReturn_HappyOrSad()
+        public void GivenMood_ShouldReturn_Sad()
         {
             string expected = "Sad";
             string message = "I am Sad";
             MoodAnalyser moodAnalyser = new MoodAnalyser();
             Assert.AreEqual(expected, moodAnalyser.AnalyseMood(message));
         }
+        [TestMethod]
+        public void GivenMethod_ShouldReturn_Happy()
+        {
+            string expected = "Happy";
+            string message = "I am Happy";
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            Assert.AreEqual(expected, moodAnalyser.AnalyseMood(message));
+        }
+
     }
 }
