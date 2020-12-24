58e5e7b4e5702602c4fa0870781b57a8f7971b02
abh224272 <abh224272@gmail.com>
2020-12-24 17:35:51 +0530

    [Abhishek Mishra] Add: Ability to Analyse mood According to Message

---
+++ MoodAnalyzer/MoodAnalyser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    class MoodAnalyser
+    {
+        public string AnalyseMood(string message)
+        {
+            if (message.Contains("Sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -7,6 +7,8 @@ namespace MoodAnalyzer
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Mood Analyser Program");
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            Console.WriteLine(moodAnalyser.AnalyseMood("I am in Happy Mood"));
         }
     }
 }
